9e421d781b07acf3862cc6b47f49c6347556cc6b
lee-vincent <vinnie@vinnielee.io>
2021-07-25 16:35:45 -0400

    drop to 2 instances

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu" {
-  count         = 3
+  count         = 2
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
