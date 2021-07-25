ccec1fd0ea9003af203769fa19066e90cb8fcae6
lee-vincent <vinnie@vinnielee.io>
2021-07-25 16:28:03 -0400

    3 instances

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu" {
-  count         = 2
+  count         = 3
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
